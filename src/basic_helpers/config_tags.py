4c38ed1a22d71fc292a314f55d1bd1e83225e45e
hgmuc <helmut.gassner.muc@gmail.com>
2026-03-05 01:01:09 +0100

    Set more specific type hints instead of generic ones

--- src/basic_helpers/config_tags.py
+++ src/basic_helpers/config_tags.py
@@ -1,78 +1,88 @@
-from typing import Union, Set, List, Dict, Tuple
+from typing import Union, Set, List, Dict, Tuple, Final, Literal
+from .types_osm import OsmHighway, OsmNodeId, HikeSurface, OsmAccess
 
-CYCLING_HGWY_EXCL: Set[str] = set([
+
+OsmLocalityTag = Literal['allotment', 'archipelago', 'borough', 'city', 'city_block', 'farm', 
+                         'hamlet', 'island', 'islet', 'isolated_dwelling', 'locality', 'municipality', 
+                         'neighbourhood', 'plot', 'quarter', 'square', 'suburb', 'town', 'village']
+
+OsmHighwaySet = set[OsmHighway]
+OsmAccessSet = set[OsmAccess]
+
+
+CYCLING_HGWY_EXCL: Final[OsmHighwaySet] = set([
     'trunk', 'trunk_link', 'raceway', 'motorway', 'motorway_link', 'proposed', 'platform', 
     'emergency_bay', 'corridor', 'bus_stop', 'escape', 'via_ferrata', 'trail', 'razed', 'ladder'])
 
-CYCLING_HGWY: Set[str] = set([
+CYCLING_HGWY: OsmHighwaySet = set([
     'residential', 'service', 'track', 'footway', 'unclassified', 'path', 'primary', 'secondary', 
     'tertiary', 'cycleway', 'living_street', 'crossing', 'pedestrian', 'road', 'rest_area', 'construction',
     'steps', 'elevator', 'primary_link', 'secondary_link', 'bridleway', 'services', 'tertiary_link', 
     'ferry', 'shuttle_train', 'shuttle_bus'])
 
-KEEP_HGWY: Set[str] = set([
+KEEP_HGWY: Final[OsmHighwaySet] = set([
     'residential', 'unclassified', 'path', 'primary', 'secondary', 'tertiary', 'cycleway', 'living_street', 
     'crossing', 'road', 'service', 'primary_link', 'secondary_link', 'tertiary_link', 'shuttle_train', 'shuttle_bus'])
 
-HIKE_HGWYS: Set[str] = set([
+HIKE_HGWYS: Final[OsmHighwaySet] = set([
     'trail', 'path', 'cycleway', 'footway', 'pedestrian', 'steps', 'bridleway'])
 
-ONEWAY_DISMOUNT_HGWYS: Set[str] = set([
+ONEWAY_DISMOUNT_HGWYS: Final[OsmHighwaySet] = set([
     'track', 'path', 'cycleway', 'footway', 'pedestrian', 'crossing', 'residential', 'living_street'])
 
-BIKE_PATHS_MED_PRIO: Set[str] = set([
+BIKE_PATHS_MED_PRIO: Final[OsmHighwaySet] = set([
     'track', 'path', 'crossing', 'footway', 'pedestrian'])
 
-BIKE_PATHS_LOW_PRIO: Set[str] = set([
+BIKE_PATHS_LOW_PRIO: Final[OsmHighwaySet] = set([
     'bridleway', 'pedestrian', 'steps', 'footway', 'elevator'])
 
-HIKE_SURFACE_GOOD: Set[str] = set([
+HIKE_SURFACE_GOOD: Final[Set[HikeSurface]] = set([
     'asphalt', 'metal', 'paved/concrete', 'plastic/rubber', 'timber', 'wood'])
 
-HIKE_SURFACE_OK: Set[str] = set([
+HIKE_SURFACE_OK: Final[Set[HikeSurface]] = set([
     'forest_soil', 'grass', 'gravel/sand', 'ground/dirt', 'ground/dirt/grass', 
     'ground/grass', 'ground/gravel', 'salt'])
 
-HIKE_SURFACE_POOR: Set[str] = set([
+HIKE_SURFACE_POOR: Final[Set[HikeSurface]] = set([
     'ground/rocks', 'ground/roots', 'rocks', 'rubble', 'scrub', 'snow/ice', 
     'stone/cobblestone'])
 
-PISTE_TYPE_HIKING: Set[str] = set([
+PISTE_TYPE_HIKING: Final[Set[str]] = set([
     'nordic', 'downhill', 'hike', 'skitour', 'nordic;hike', 'snowshoe', 
     'hike;nordic', 'hike;snowshoe', 'nordic;hike;snowshoe', 'nordic;skitour', 
     'alpine', 'snowshoe;fatbike', 'backcountry', 'hike;skitour', 'hiking'])
 
-PISTE_TYPE_BICYCLE: Set[str] = set([
+PISTE_TYPE_BICYCLE: Final[Set[str]] = set([
     'nordic', 'sled', 'connection', 'fatbike', 'sleigh', 'nordic;fatbike', 
     'fatbike;hike', 'nordic;fatbike;hike', 'hike;fatbike', 'nordic;hike;fatbike'])
 
-PARKING_NO_ACCESS: Set[str] = set(['private', 'no', 'customers', 'delivery', 'limited'])
+PARKING_NO_ACCESS: Final[OsmAccessSet] = set(['private', 'no', 'customers', 'delivery', 'limited'])
 
-SUMMER_LS: Set[str] = set(['summer', 'yes', 'yes|summer'])
+SUMMER_LS: Final[Set[str]] = set(['summer', 'yes', 'yes|summer'])
 
-SERVICES_INVALID: Set[str] = set([
+SERVICES_INVALID: Final[Set[str]] = set([
     'drive-through', 'drive_through', 'parking', 'emergency_access', 'industrial', 'camp_site', 
     'traffic_park', 'pipestem', 'quarry', 'training', 'test_track',  'bus', 'busway', 'repair', 'dealer',
     'gps', 'service_station', 'service_road', 'fuel', 'weigh_station', 'bus_rapid_transit', 'private', 
     'allotments', 'cemetery', 'bypass', 'military', 'car_wash', 'delivery', 'garages', 
     'waterway', 'slipway', 'railway'])
 
-HGWY_TAGS: Set[str] = set([
+HGWY_TAGS: Final[Set[str]] = set([
     "highway", "area", "man_made", "informal", "access", "locked", "blocked", "gate_locked", "tracktype", 
     "surface", "smoothness", "junction", "maxspeed", "lanes", "oneway", "temporary:oneway", "service", 
     'motor_vehicle', 'vehicle', 'motorcar', 'toll', 'ford', 'incline', 'living_street', "scenic", 
     'traffic', "services", "footway", "crossing", "crossing_ref", "crossing:island", "route", "bridge", 
     "tunnel", "layer", "aerialway"])
 
-PERMISSIBLE_STATES: Set[str] = set([
+PERMISSIBLE_STATES: Final[OsmAccessSet] = set([
     'yes', 'Yes', 'designated', 'permissive', 'official', 'passable', 'fwd', 'bwd'])
 
-NOT_PERMISSIBLE_STATES: Set[str] = set([
+NOT_PERMISSIBLE_STATES: Final[OsmAccessSet] = set([
     'no', 'private', 'discouraged', 'use_sidepath', 'impassable', 'destination', 
     'military', 'forestry', 'delivery', 'customers', 'students', 'construction', 
     'employees', 'emergency', 'limited', 'private;customers', 'customers;private'])
 
-DISUSED_HIGHWAYS: Set[str] = set([
+DISUSED_HIGHWAYS: Final[Set[str]] = set([
     'abandoned:highway', 'disused:highway', 'demolished:highway', 'closed:highway',
     'was:highway', 'razed:highway', 'removed:highway', 'destroyed:highway', 
     'construction:highway', 'not:highway', 'historic:highway', 'no:highway', 
@@ -84,15 +94,16 @@ DISUSED_HIGHWAYS: Set[str] = set([
     'disused:route', 'abandoned:route', 'abandoned:bridge',
     'destroyed:bridge', 'was:bridge', 'disused:bridge', 'razed:bridge',])
 
-FUTURE_HIGHWAYS: Set[str] = set(['proposed:highway', 'planned:highway'])
+FUTURE_HIGHWAYS: Final[Set[str]] = set(['proposed:highway', 'planned:highway'])
 
 
-NATURAL_REGION: Set[str] = set(['mountain_range', 'mountain_area', 'tourism', 'valley'])
+NATURAL_REGION: Final[Set[str]] = set(['mountain_range', 'mountain_area', 'tourism', 'valley'])
+#NATURAL_REGION: Set[str] = set(['mountain_range', 'mountain_area', 'tourism', 'valley'])
 
-RAILWAY_LS: List[str] = ['rail', 'subway', 'narrow_gauge', 'funicular']
+RAILWAY_LS: Final[List[str]] = ['rail', 'subway', 'narrow_gauge', 'funicular']
 
 
-CYCLE_TAGS: Set[str] = set([
+CYCLE_TAGS: Final[Set[str]] = set([
     "bicycle", "bicycle:forward", "bicycle:backward", "bicycle:lanes:forward", "mtb:scale", 
     "bicycle:lanes:backward", "routing:bicycle", "shoulder:bicycle", "oneway:bicycle", "moped",
     "cycleway", "cycleway:bicycle", "cycleway:lane", "cycleway:oneway", "cycleway:smoothness", 
@@ -110,7 +121,7 @@ CYCLE_TAGS: Set[str] = set([
     "sidewalk:bicycle", "sidewalk:right:bicycle", "sidewalk:left:bicycle", "sidewalk:both:bicycle"])
      
 # HINWEIS: Elemente in dieser Liste müssen auch zu HGWY_TAGS oder CYCLE_TAGS hinzugefügt werden # 
-TAG_MAP_DEP_ORDER: List[str] = [
+TAG_MAP_DEP_ORDER: Final[List[str]] = [
     "railway", 'path', 'tracktype', 'mtb:scale', 'mtb:scale:uphill', 'mtb:scale:imba', 
     'mtb:scale:type', "ford", "is_sidepath", 'is_sidepath_of', 'is_sidepath:of', 'is_sidepath:ref', 
     'is_sidepath:of:ref', 'is_sidepath:of:name', "indoor", "covered", 'services', 'service', 'smoothness',
@@ -127,24 +138,24 @@ TAG_MAP_DEP_ORDER: List[str] = [
     "hike_surface", "hike_smoothness", "hike_overgrown", 
     "hike_condition", "sidewalk", "sidewalk:right", "sidewalk:left", "sidewalk:both"]
 
-SAC_SCALE_VALS: List[str] = [
+SAC_SCALE_VALS: Final[List[str]] = [
     'strolling', 'hiking', 'mountain_hiking', 'demanding_mountain_hiking', 'alpine_hiking', 
     'demanding_alpine_hiking', 'difficult_alpine_hiking']
 
-CLS_BICYCLE_MAP: Dict[Union[str, int], int] = {
+CLS_BICYCLE_MAP: Final[Dict[Union[str, int], int]] = {
     '-3': -3, '-2': -2, '-1': -1, '0': 0, '1': 1, '2': 2, '3': 3, 
     -3: -3, -2: -2, -1: -1, 0: 0, 1: 1, 2: 2, 3: 3}
 
-CLS_BICYCLE_TAGS: List[str] = ["class:bicycle", "class:bicycle:touring", "class:bicycle:commute"]
+CLS_BICYCLE_TAGS: Final[List[str]] = ["class:bicycle", "class:bicycle:touring", "class:bicycle:commute"]
 # class:bicycle:trailer im Moment außen vor, da nur 20 Ways eine negative Bewertung haben
-CLS_ROADBIKE_TAGS: List[str] = ["class:roadbike", "class:bicycle:roadcycling"]
-CLS_MTB_TAGS: List[str] = ["class:bicycle:mtb", "class:bicycle:mtb:technical"]
+CLS_ROADBIKE_TAGS: Final[List[str]] = ["class:roadbike", "class:bicycle:roadcycling"]
+CLS_MTB_TAGS: Final[List[str]] = ["class:bicycle:mtb", "class:bicycle:mtb:technical"]
 
-MTB_TYPES: Set[str] = set([
+MTB_TYPES: Final[Set[str]] = set([
     'crosscountry', 'allmountain', 'enduro', 'downhill', 'freeride', 'dirtjump', 'pumptrack', 'pumptrail', 
     'xc', 'slopestyle', 'bikepark', 'uphill', 'dh', '0', '1', '2', 'pump_track', 'northshore'])
-MTB_TYPES_DOWNHILL: List[str] = ['downhill', 'freeride', 'slopestyle', 'dh', 'bikepark', 'northshore']
-MTB_TYPES_UPHILL: List[str] = ['uphill', 'xc', 'crosscountry', 'allmountain', 'enduro', '0', '1', '2']
+MTB_TYPES_DOWNHILL: Final[List[str]] = ['downhill', 'freeride', 'slopestyle', 'dh', 'bikepark', 'northshore']
+MTB_TYPES_UPHILL: Final[List[str]] = ['uphill', 'xc', 'crosscountry', 'allmountain', 'enduro', '0', '1', '2']
 
 #NO_ACCESS = ['no', 'private', 'restricted', 'discouraged', 'customer', 'delivery', 'limited', 'bus', 
 #             'construction', 'guest', 'employee', 'licence', 'military', 'locked', 'resident', 'emergency']
@@ -166,52 +177,52 @@ IS_WATER_TUNNEL_LIST = set(['yes', 'culvert', 'pipe', 'flooded', 'drain', 'chann
 #                   'tiger': 'traffic_signals'}
 
 ## TOURISM et al
-TOURISM_TYPES: Set[str] = set([
+TOURISM_TYPES: Final[Set[str]] = set([
     'viewpoint', 'artwork', 'monument', 'memorial', 'zoo', 'aquarium', 'wine_cellar', 'winery', 
     'attraction', 'hotel', 'guest_house', 'hostel', 'motel', 'camp_site', 'alpine_hut']) 
 
-TOURISM_SWIMMING: Set[str] = set(["swimming_area", "water_park", "beach_resort"])
+TOURISM_SWIMMING: Final[Set[str]] = set(["swimming_area", "water_park", "beach_resort"])
                     # wilderness_hut <=> ist Gegensatz zu den anderen Optionen unbewirtschaftet
 
-HISTORIC_TYPES: Set[str] = set([
+HISTORIC_TYPES: Final[Set[str]] = set([
     'castle', 'building', 'monument', 'memorial', 'archaelogical_site', 'ruins', 
     'wayside_cross', 'wayside_shrine', 'wayside_chapel', 'city_walls', 'city_gate', 
     'roman_road', 'aqueduct', 'monastery', 'chapel', 'church'])
 
-AMENITY_TYPES: Set[str] = set([
+AMENITY_TYPES: Final[Set[str]] = set([
     'biergarten', 'drinking_water', 'fountain', 'cafe', 'bicycle_rental', 'charging_station',
     'ice_cream', 'bakery', 'shelter'])    # place_of_worship, restaurant <=> nicht relevant
 
 BICYCLE_RENTAL_EXCL: Set[Union[str, None]] = {None, 'docking_station', 'shop', 'dropoff_point', 'yes'}
 
-BUILDING_TYPES: Set[str] = set([
+BUILDING_TYPES: Final[Set[str]] = set([
     'church', 'castle', 'chapel', 'stadium', 'tower', 'cathedral', 'monastery', 
     'marketplace', 'museum', 'villa', 'bakehouse'])
 
-REL_LOCALITY_TYPES: Set[str] = set([
+REL_LOCALITY_TYPES: Final[Set[str]] = set([
     'village', 'locality', 'hamlet', 'islet', 'isolated_dwelling', 'farm', 'plot', 'neighbourhood', 
     'suburb', 'quarter', 'town', 'island', 'city_block', 'square', 'city', 'municipality'])
 
 ## Guideposts
-DIRECTION_LIST: List[Tuple[str, str]] = [
+DIRECTION_LIST: Final[List[Tuple[str, str]]] = [
     ('N', '_north'), ('NE', '_northeast'), ('E', '_east'), ('SE', '_southeast'), ('S', '_south'), 
     ('SW', '_southwest'), ('W', '_west'), ('NW', '_northwest')]
 
-DIR_EXCL_LIST: Set[str] = set([
+DIR_EXCL_LIST: Final[Set[str]] = set([
     'NORTH', 'EAST', 'WEST', 'SOUTH', 'NORTHEAST', 'SOUTHEAST', 'NORTHWEST', 'SOUTHWEST', 
     'W', 'N', 'E', 'S', 'NW', 'SW', 'NE', 'SE', 'NNW', 'NNE', 'SSW', 'SSE', 'WNW', 'ENE', 'WSW', 'ESE',
     'FORWARD', 'BACKWARD', 'CLOCKWISE', 'ANTICLOCKWISE'])
 
-REL_MEMBER_ROLES: Set[str] = set([
+REL_MEMBER_ROLES: Final[Set[str]] = set([
     'guidepost', 'giudepost', 'guidepost ', 'gudiepost', 'guidepost', 'route_marker', 
     'info', 'information', 'information:board', 'information_board', 'map'])
 
-NETWORK_KEYS: Set[str] = set(['rcn', 'lcn', 'ncn', 'bicycle', 'rcn;rwn', 'rwn;rcn', 'lcn;lwn', 'lwn;lcn'])
+NETWORK_KEYS: Final[Set[str]] = set(['rcn', 'lcn', 'ncn', 'bicycle', 'rcn;rwn', 'rwn;rcn', 'lcn;lwn', 'lwn;lcn'])
 
-HIKING_NETWORK_KEYS: Set[str] = set([
+HIKING_NETWORK_KEYS: Final[Set[str]] = set([
     'rwn', 'lwn', 'nwn', 'rcn;rwn', 'rwn;rcn', 'lcn;lwn', 'lwn;lcn', 'rwn;lwn', 'nwn;lwn'])
 
-GP_LIST: Set[str] = {'guidepost', 'route_marker', 'map'}
+GP_LIST: Final[Set[str]] = {'guidepost', 'route_marker', 'map'}
 
 ## Shops and vending machines (=> doch eher über OVERPASS wg Komplexität und Dauer <=> Nodes, Ways, Relations)
 #SHOP_TYPES = set(["bakery", "convenience", "supermarket", "grocery", "dairy", "cheese", "kiosk", "food", "bicycle", "confectionery", 
@@ -226,55 +237,56 @@ GP_LIST: Set[str] = {'guidepost', 'route_marker', 'map'}
 
 # Highways and waterways
 # WATERWAY_TYPES = ['river', 'stream', 'canal', 'ditch', 'rapids']
-WATERWAY_TYPES: Dict[str, int] = {'river': 1, 'canal': 2, 'rapids': 3, 'stream': 4, 'ditch': 5}
+WATERWAY_TYPES: Final[Dict[str, int]] = {'river': 1, 'canal': 2, 'rapids': 3, 'stream': 4, 'ditch': 5}
 
-HIGHWAY_TYPES: Set[str] = {
+HIGHWAY_TYPES: Final[OsmHighwaySet] = {
     'trunk', 'trunk_link', 'raceway', 'motorway', 'motorway_link', 'primary', 
     'primary_link', 'secondary', 'secondary_link', 'tertiary', 'tertiary_link', 'road'}
 
-FOOTWAY_TYPES: Set[str] = {
+FOOTWAY_TYPES: Final[Set[str]] = {
     'sidewalk', 'path', 'yes', 'link', 'designated', 'track', 'footway', 'pedestrian', 
     'shortcut', 'service', 'trail', 'footpath', 'boardwalk', 'crossing'}
 
-CROSSING_TYPES: Set[str] = {'crossing', 'traffic_island', 'island'}
-HIKING_HGWYS: Set[str] = {'trail', 'via_ferrata', 'bus_stop', 'ladder'}
+CROSSING_TYPES: Final[Set[str]] = {'crossing', 'traffic_island', 'island'}
+HIKING_HGWYS: Final[OsmHighwaySet] = {'trail', 'via_ferrata', 'bus_stop', 'ladder'}
 
-highway2id: Dict[str, int] = {hgwy: id for hgwy, id in zip(HIGHWAY_TYPES, range(len(HIGHWAY_TYPES)))}
+highway2id: Final[Dict[OsmHighway, int]] = {hgwy: id for hgwy, id in zip(HIGHWAY_TYPES, range(len(HIGHWAY_TYPES)))}
 
-WATERWAY_MEM_ROLES: Set[str] = set([
+WATERWAY_MEM_ROLES: Final[Set[str]] = set([
     'main_stream', 'side_stream', 'anabranch', 'tributary', 'waterbody', 'distributary'])
 
 # WATER_TYPE_PRIO und WATERWAY_TYPES werden genutzt, um übergeordneten Typen bei der Benennung der Wasserflächen zu priorisieren
 # z.B. See vor Fluss, Fluss vor Bach etc.
-WATER_TYPE_PRIO: Dict[Union[str, None], int] = {
+WATER_TYPE_PRIO: Final[Dict[Union[str, None], int]] = {
     'lake': 0, 'lagoon': 0, 'shallow': 0, 'wastewater': 0, 'bay':0, 'reservoir': 1, 'harbour': 1, 
     'basin': 1, 'pond': 1, 'fishpond': 1, 'oxbow': 1, 'moat': 1, 'beach': 50, 'lock': 50, None: 99}
 for k, v in WATERWAY_TYPES.items():
     WATER_TYPE_PRIO[k] = v
 
-HIGHWAY_NODES: Set[str] = set([
+HIGHWAY_NODES: Final[Set[str]] = set([
     "elevator", "traffic_signals", "crossing", "turning_circle", "stop", "give_way", "footway",
     "turning_loop", "mini_roundabout", "traffic_signals;crossing", "crossing;traffic_signals"])
 
-FOOT_BARRIER_LS: Set[str] = set([
+FOOT_BARRIER_LS: Final[Set[str]] = set([
     "kissing_gate", "footgate", "stile", "v_stile", "turnstile", "squeeze", "squeeze_stile", 
     "cycle_barrier", "bicycle_barrier", "horse_stile", "horse_jump", "step_over", "horse_barrier", 
     "cattle_grid", "motorcyle_barrier", "bollard", "car_barrier", "car_trap"])
 
 # Residential areas <=> localities: list of relevant locality types:
-LOCALITY_TAG_VALS: Set[str] = set([
+
+LOCALITY_TAG_VALS: Final[Set[OsmLocalityTag]] = set([
     'borough', 'city', 'city_block', 'hamlet', 'island', 'isolated_dwelling', 'locality', 
     'municipality', 'neighbourhood', 'quarter', 'suburb', 'town', 'village', 'farm'])
-LRG_LOCALITY_TAG_VALS: Set[str] = {'city', 'island', 'municipality', 'town', 'village'}
+LRG_LOCALITY_TAG_VALS: Final[Set[OsmLocalityTag]] = {'city', 'island', 'municipality', 'town', 'village'}
 # farm = isolated_dwelling (Einzelgehöfte oder alleinstehende Häuser)
-LOCALITY_TAG_VALS_NOT_USED: Set[str] = {'archipelago', 'islet', 'plot', 'square', 'allotment'}
+LOCALITY_TAG_VALS_NOT_USED: Final[Set[OsmLocalityTag]] = {'archipelago', 'islet', 'plot', 'square', 'allotment'}
 # archipelago - uninterressant, weil aus mehreren (idR) benannten Inseln bestehend (z.B. Kanaren vs Teneriffa)
 # islet = zu klein
 # plot, allotments = idR unbewohnt
 # square = Name idR Teil des Straßennamens
 
 # Overriding misleading / missing tourism = VIEWPOINT tags (had tourism=information instead)
-VIEWPTS_NODES: Set[int] = set([
+VIEWPTS_NODES: Set[OsmNodeId] = set([
     6388605431, 385295165, 3792706718, 8464898307, 11023622894, 730630702, 3785884608, 7906230694, 
     1349844023, 1350936369, 3268274619, 3883131138, 6925480834, 7289441637, 8443656122, 9192874824, 
     11383644401, 11502684355, 11508674469, 11508672384, 11499568853, 10037901873, 10017518135, 
